3f2aa465a6408e7ca4eb03f692bee317b47e8345
iutkarsh077 <utkarshsingh132003@gmail.com>
2026-06-07 20:58:13 +0530

    practice sessions

---
+++ practpython.py
@@ -0,0 +1,61 @@
+# fruits = ["apple", "banana", "cherry", "Pineapple"]
+
+
+# first, second, third = fruits
+
+# print(first, second, third)
+
+
+# x = 90
+# def MyFunc():
+#     global x
+#     x = 56
+#     print("Inside function", x, "\n")
+
+# MyFunc()
+# print("In Global: ", x)
+
+# x = range(10)
+# print(list(x))
+
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+
+# print(a)
+
+# x = "banana"
+
+# for char in x:
+#     print(char)
+
+
+# text = "The best things in life are free!"
+
+# if "free" in text:
+#     print("Yes, available")
+
+# print(text[1:5:2])
+
+# print(text.upper())
+# print(text)
+
+# text = "Hello, World!"
+
+# print(text.replace("o", "J"))
+# print(text)
+
+list1 = ["Apple", "banana", "Cherry"]
+# list1.extend("Mango")
+
+# print(list1)
+
+# list1.remove("banana")
+# print(list1)
+
+list2 = list1
+list2[1] = "Utkarsh"
+print(list1)
+print(list2)
